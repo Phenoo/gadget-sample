1ab2d421150df95bad20628e085e7ed5cb2a6b33
Phenoo <phenopreneur@outlook.com>
2023-04-18 00:02:19 +0200

    img correct

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import NavigationBar from '../Navbar'
 import FooterSection from '../Footer'
+import Head from 'next/head'
 
 
 const Layout = ({children}) => {
   return (
     <>
+    <Head>
+      <title>Gadgeett</title>
+    </Head>
       <NavigationBar />
       <main>
         {children}
